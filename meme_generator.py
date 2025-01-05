ab476a21b14c05a3ded1cbc0f9660f62c0914e28
Tatetrix <tranvietquoctoan123@gmail.com>
2025-01-05 17:49:06 +0700

    first commit

---
+++ meme_generator.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("Hello world")
+pressed = st.button("Press me")
+print(pressed)
+with st.echo():
+    def pick_odd(list):
+       pass
+st.metric("My metric", 42, -2)
+st.altair_chart(1)
+
+    
